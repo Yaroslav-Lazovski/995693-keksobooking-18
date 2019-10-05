7eab2ffaab63104409b4b2bd1c5bc1118d49571d
Yaroslav-Lazovski <50278946+Yaroslav-Lazovski@users.noreply.github.com>
2019-10-05 18:13:58 +0300

    module3-task3

--- js/main.js
+++ js/main.js
@@ -19,12 +19,12 @@ var chooseRandom = function (arr) {
   return arr[rand];
 };
 
-var getPhotos = function (offerTitle) {
+var getPhotos = function () {
   var photoCount = getRandomBetween(1, 5);
   var photos = [];
 
   for (var i = 0; i < photoCount; i++) {
-    photos.push('http://github.io/assets/images/tokyo/hotel' + offerTitle + i + '.jpg');
+    photos.push('http://github.io/assets/images/tokyo/hotel' + i + '.jpg');
   }
 
   return photos;
@@ -35,18 +35,18 @@ var getOffer = function (i) {
     'x': getRandomBetween(0, mapWidth),
     'y': getRandomBetween(130, 630),
   };
-  var title = 'Предложение №' + i;
-  var user = i + 1;
+  var userCount = i + 1;
+  var title = 'Предложение №' + userCount;
 
   return {
     'author': {
-      'avatar': 'img/avatars/user0' + user + '.png',
+      'avatar': 'img/avatars/user0' + userCount + '.png',
     },
     'location': location,
     'offer': {
       'title': title,
       'address': 'Координаты : (' + location.x + ' ' + location.y + ')',
-      'price': getRandomBetween(1, 5000) * 1000,
+      'price': getRandomBetween(1, 5000) * 10,
       'type': chooseRandom(TYPES),
       'rooms': getRandomBetween(1, 9),
       'guests': getRandomBetween(0, 10),
@@ -92,7 +92,6 @@ var createPin = function (offer) {
   return pinButton;
 };
 
-
 var renderPins = function (offers) {
   var fragment = document.createDocumentFragment();
 
@@ -107,3 +106,57 @@ var renderPins = function (offers) {
 
 var offers = getOffers(8);
 renderPins(offers);
+
+var cardTemplate = document.querySelector('#card').content.querySelector('article');
+var cardElement = cardTemplate.cloneNode(true);
+
+var popupPhotos = cardElement.querySelector('.popup__photos');
+var createPhoto = function () {
+  var photoFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < offers[0].offer.photos.length; i++) {
+    var popupImg = document.createElement('img');
+
+    popupImg.setAttribute('src', offers[0].offer.photos[i]);
+    popupImg.setAttribute('class', 'popup__photo');
+    popupImg.setAttribute('width', '45');
+    popupImg.setAttribute('height', '40');
+    popupImg.setAttribute('alt', 'Фотография жилья');
+
+    photoFragment.appendChild(popupImg);
+
+  }
+  popupPhotos.appendChild(photoFragment);
+};
+
+var createAd = function () {
+  cardElement.querySelector('.popup__title').textContent = offers[0].offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offers[0].offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = offers[0].offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__text--capacity').textContent = offers[0].offer.rooms + ' комнаты для ' + offers[0].offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offers[0].offer.checkin + ', выезд до ' + offers[0].offer.checkout;
+  cardElement.querySelector('.popup__features').textContent = offers[0].offer.features;
+  cardElement.querySelector('.popup__description').textContent = offers[0].offer.description;
+  cardElement.querySelector('.popup__avatar').src = offers[0].author.avatar;
+  popupType();
+  createPhoto();
+
+  // eslint-disable-next-line no-console
+  console.log(cardElement);
+};
+
+var popupType = function () {
+  for (var i = 0; i < offers[0].offer.type.length; i++) {
+    if (chooseRandom(TYPES) === 'palace') {
+      cardElement.querySelector('.popup__type').textContent = 'Дворец';
+    } else if (chooseRandom(TYPES) === 'flat') {
+      cardElement.querySelector('.popup__type').textContent = 'Квартира';
+    } else if (chooseRandom(TYPES) === 'house') {
+      cardElement.querySelector('.popup__type').textContent = 'Дом';
+    } else if (chooseRandom(TYPES) === 'bungalo') {
+      cardElement.querySelector('.popup__type').textContent = 'Бунгало';
+    }
+  }
+};
+
+createAd();
